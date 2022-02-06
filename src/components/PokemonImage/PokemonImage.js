90afa3247a9013b6762eae9ad3b72368e980a2fa
sorinNgit <sorinnsg@gmail.com>
2022-02-06 12:48:17 +0200

    Components + utils for pokedex page

---
+++ src/components/PokemonImage/PokemonImage.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {padNumber} from '../../utils';
+import {BASENAME} from '../../Constants';
+
+const fixNames = name => {
+  if (name === 'sceptile') {
+    return 'sceptille';
+  }
+  if (name === 'jirachi') {
+    return 'lirachi';
+  }
+  if (name === 'ninjask') {
+    return 'niniask';
+  }
+  if (name === 'baltoy') {
+    return 'baltov';
+  }
+  if (name === 'claydol') {
+    return 'clavdol';
+  }
+  if (name.includes('deoxys')) {
+    return 'deoxys';
+  }
+  if (name === 'unown') {
+    return `${name}-a`;
+  }
+  if (name === 'spinda') {
+    return `${name}-1`;
+  }
+  if (name === 'castform') {
+    return `${name}-normal`;
+  }
+  if (name === 'burmy') {
+    return `${name}-plant`;
+  }
+  if (name === 'vespiquen') {
+    return 'vespiqueen';
+  }
+  if (name === 'cherrim') {
+    return 'cherim-overcast';
+  }
+  if (name === 'shellos') {
+    return 'shellos-east';
+  }
+  if (name === 'gastrodon') {
+    return 'gastrodon-east';
+  }
+  if (name === 'mime-jr') {
+    return 'mimejr';
+  }
+  if (name === 'lickilicky') {
+    return 'lickilicki';
+  }
+  if (name === 'ho-oh') {
+    return 'hooh';
+  }
+  return name;
+};
+
+const PokemonImage = ({id, name, size = 100, animate = false}) => (
+  <div style={{padding: 20}}>
+    <img
+      src={`${BASENAME}/pokemons/${padNumber(id, 3)}-${fixNames(name)}.svg`}
+      style={{height: size, width: size}}
+      alt={name}
+    />
+  </div>
+);
+
+export default PokemonImage;
---
+++ src/utils.js
@@ -0,0 +1,45 @@
+/* eslint-disable id-length */
+const padNumber = (num, size = 3) => {
+    let str = num.toString();
+    while (str.length < size) {
+      str = '0' + str;
+    }
+    return str;
+  };
+  
+  const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
+  
+  const deepEqual = (a, b) => {
+    if (
+      typeof a === 'object' &&
+      a != null &&
+      (typeof b === 'object' && b != null)
+    ) {
+      const count = [0, 0];
+      //eslint-disable-next-line no-unused-vars
+      for (const _ in a) {
+        count[0]++;
+      }
+      //eslint-disable-next-line no-unused-vars
+      for (const _ in b) {
+        count[1]++;
+      }
+      if (count[0] - count[1] !== 0) {
+        return false;
+      }
+      for (const key in a) {
+        if (!(key in b) || !deepEqual(a[key], b[key])) {
+          return false;
+        }
+      }
+      for (const key in b) {
+        if (!(key in a) || !deepEqual(b[key], a[key])) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return a === b;
+  };
+  
+  export {padNumber, capitalize, deepEqual};
